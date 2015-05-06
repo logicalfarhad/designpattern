6c4b9bf9dc8644192abeb6d0d600eaf82dda22aa
logicalforhad <forhad.univdhaka@gmail.com>
2015-05-06 13:51:08 +0600

    Decorator Init

---
+++ GofPattern/Decorator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Decorator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] arr = { "One", "Two", "Three", "Five" };
+            List<String> list = arr.ToList();
+            arr.Where(x=>!x.Equals("Five")).ToList().ForEach(Console.WriteLine);
+            Console.WriteLine();
+            arr.TakeWhile(x => !x.Equals("Five")).ToList().ForEach(Console.WriteLine);
+            Console.ReadLine();
+        }
+    }
+}
