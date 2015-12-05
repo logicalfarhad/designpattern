5f13b728034801e0c69cc327a00886f996d47708
logicalforhad <forhad.univdhaka@gmail.com>
2015-12-05 20:16:30 +0600

    Decorator design pattern project added

---
+++ GofPattern/DecoratorPattern/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DecoratorPattern
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            Console.Read();
+        }
+    }
+}
---
+++ GofPattern/DecoratorPattern/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DecoratorPattern")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DecoratorPattern")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0743a89f-562b-4507-9ef2-6606bfaf1f7d")]
